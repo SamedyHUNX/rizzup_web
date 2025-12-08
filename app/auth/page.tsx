0f92a4899a668897b4cca4e2def020e81ad9aa4c
samedyhunx <samedy.hunx@gmail.com>
2025-12-08 15:45:35 +0700

    feature/supabase: supabase signin and signup

---
+++ app/auth/page.tsx
@@ -0,0 +1,129 @@
+"use client";
+
+import { useAuth } from "@/contexts/auth-context";
+import { createClient } from "@/lib/supabase/client";
+import { useRouter } from "next/navigation";
+import { FormEvent, useEffect, useState } from "react";
+
+export default function AuthPage() {
+  const [isSignUp, setIsSignUp] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+  const supabase = createClient();
+  const { user, loading: authLoading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user && !authLoading) {
+      router.push("/");
+    }
+  }, [user, authLoading, router]);
+
+  async function handleAuth(e: FormEvent) {
+    e.preventDefault();
+
+    setLoading(true);
+    setError("");
+
+    try {
+      if (isSignUp) {
+        const { data, error } = await supabase.auth.signUp({
+          email,
+          password,
+        });
+
+        if (error) throw error;
+        if (data.user && data.session) {
+          setError("Please check your email for a confirmation link");
+          return;
+        }
+      } else {
+        const { error } = await supabase.auth.signInWithPassword({
+          email,
+          password,
+        });
+        if (error) throw error;
+      }
+    } catch (error: any) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-pink-100 to-red-100 dark:from-gray-900 dark:to-gray-800">
+      <div className="max-w-md w-full space-y-8 p-8">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold text-gray-900 dark:text-white mb-2">
+            StreamMatch
+          </h1>
+          <p className="text-gray-600 dark:text-gray-400">
+            {isSignUp ? "Create Your Account" : "Sign in to your account"}
+          </p>
+        </div>
+
+        <form className="space-y-6" onSubmit={handleAuth}>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+            >
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter your email"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:ring-pink-500 focus:border-pink-500 dark:bg-gray-800 dark:text-white"
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+            >
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter your password"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:ring-pink-500 focus:border-pink-500 dark:bg-gray-800 dark:text-white"
+            />
+
+            {error && (
+              <div className="text-red-600 dark:text-red-400 text-sm">
+                {error}
+              </div>
+            )}
+          </div>
+          <button
+            type="submit"
+            disabled={loading}
+            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-linear-to-r from-pink-500 to-red-500 hover:from-pink-600 hover:to-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500 disabled:opacity-50"
+          >
+            {loading ? "Loading..." : isSignUp ? "Sign Up" : "Sign In"}
+          </button>
+        </form>
+
+        <div className="text-center">
+          <button onClick={() => setIsSignUp(!isSignUp)}>
+            {isSignUp
+              ? "Already have an account? Sign In"
+              : "Dont't have an account? Sign Up"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ contexts/auth-context.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { createClient } from "@/lib/supabase/client";
+import { User } from "@supabase/supabase-js";
+import { error } from "console";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+interface AuthContextType {
+  user: User | null;
+  loading: boolean;
+  signOut: () => Promise<void>;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const supabase = createClient();
+
+  useEffect(() => {
+    async function checkUser() {
+      try {
+        const {
+          data: { session },
+        } = await supabase.auth.getSession();
+
+        setUser(session?.user ?? null);
+
+        console.log(session?.user);
+
+        const {
+          data: { subscription },
+        } = supabase.auth.onAuthStateChange(async (event, session) => {
+          setUser(session?.user ?? null);
+        });
+
+        return () => subscription.unsubscribe();
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    checkUser();
+  }, []);
+
+  async function signOut() {
+    try {
+      await supabase.auth.signOut();
+    } catch {
+      console.error("Error signing out: ", error);
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, loading, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+}
