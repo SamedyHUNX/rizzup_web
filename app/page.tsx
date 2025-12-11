bd94304f579c2098bbef294afd5023f52374151d
samedyhunx <samedy.hunx@gmail.com>
2025-12-11 17:01:05 +0700

    feature/auth: protect home route

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+
+import { useAuth } from "@/contexts/auth-context";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const { user } = useAuth();
+  const router = useRouter();
+
+  if (!user) {
+    router.replace("/auth/sign-in");
+  }
+
   return <main>Home</main>;
 }
